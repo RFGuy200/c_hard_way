a3c0181b52bd5cf94b17ac63f818feef07997c4a
alex <akornishev@gmail.com>
2016-10-12 08:00:13 -0400

    working through glob

--- pnt.c
+++ pnt.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <glob.h>
 
-glob_t* pnt_create(const char *exp)
+glob_t* list_create(const char *exp)
 {
 	glob_t *pnt = malloc(sizeof(glob_t));
 
@@ -12,15 +12,35 @@ glob_t* pnt_create(const char *exp)
 	return pnt;
 }
 
+void match(glob_t globlist)
+{
+	for(i = 0; globlist->gl_pathv[i] != NULL; i++){
+		fp = fopen(globlist->gl_pathv[i], "r");
+			while(fscanf(fp, "%999[^\n]\n", line) == 1){
+				
+			
+					
+		fclose(fp);
+	}
+
+
 
 int main(int argc, char *argv[])
 {
 	int i = 0;
 	glob_t *globlist;
-	globlist = pnt_create( "*.c");
+	FILE *fp = NULL;
+	char line[1000];
 
-	for(i = 0; globlist->gl_pathv[i] != NULL; i++)
-	printf("%s\n", globlist->gl_pathv[i]);
+
+	globlist = list_create( "*.c");
+	match(globlist);
+
+	
+	
+/*	for(i = 0; globlist->gl_pathv[i] != NULL; i++)
+		printf("%s\n", globlist->gl_pathv[i]);*/
+
 	free(globlist);
 
 	return 0;
