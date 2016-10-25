0bd1e41be3c33dd86af464798aaec9152ff0575f
alex <akornishev@gmail.com>
2016-10-25 07:50:04 -0400

    getting dynamic libs now

--- ex29.c
+++ ex29.c
@@ -2,25 +2,25 @@
 #include "dbg.h"
 #include <dlfcn.h>
 
-typedef int (*lib_function) (const char *data)
+typedef int (*lib_function) (const char *data);
 
 int main(int argc, char *argv[])
 {
 	int rc = 0;
 	check(argc == 4, "USAGE: ex29 libex29.so function data");
 
-	char lib_file = argv[1];
-	char func_to_run = argv[2];
-	char data = argv[3];
+	char *lib_file = argv[1];
+	char *func_to_run = argv[2];
+	char *data = argv[3];
 
-	void *lib = dlopen(lib_file, RTDL_NOW);
+	void *lib = dlopen(lib_file, RTLD_NOW);
 	check(lib != NULL, "Failed to open the library %s: %s\n", lib_file, dlerror());
 
-	lib_func = dlsym(lib, func_to_run);
-	check(lib_func != NULL, "Did not find function %s in the library %s: %s\n", func_to_run, lib_file, dlerror());
+	lib_function func = dlsym(lib, func_to_run);
+	check(func != NULL, "Did not find function %s in the library %s: %s\n", func_to_run, lib_file, dlerror());
 
 	rc = func(data);
-	check(rc != 0, "Function %s returned %d for data:%s\n", func_to_run, rc, data);
+	check(rc == 0, "Function %s returned %d for data:%s\n", func_to_run, rc, data);
 
 	rc = dlclose(lib);
 	check(rc == 0, "Failed to close %s\n", lib_file);
--- libex29.c
+++ libex29.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <dbg.h>
+#include "dbg.h"
 
 int print_a_message(const char *msg)
 {
