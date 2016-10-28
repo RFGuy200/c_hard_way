836a8410c42a3f737cb0e9865adeb52e746d4596
alex <akornishev@gmail.com>
2016-10-28 07:52:58 -0400

    trying to see difference

--- minunit.h
+++ minunit.h
@@ -10,7 +10,7 @@
 
 #define mu_assert(test,message) if(!(test)) {log_err(message);return message;}
 
-#define mu_run_test(test) debug("\n--------%s", " " #test);message=test();test_run++;if(message) return message;
+#define mu_run_test(test) debug("\n--------%s", " " #test);message=test();tests_run++;if(message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]){\
 	argc = 1;\
