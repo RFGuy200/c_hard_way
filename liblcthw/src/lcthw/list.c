945a069eb8873a3ecc2fb1c00f769e38efbd5638
alex <akornishev@gmail.com>
2016-11-03 07:55:06 -0400

    going through double linked list

--- liblcthw/src/lcthw/list.c
+++ liblcthw/src/lcthw/list.c
@@ -9,7 +9,7 @@ List *List_create()
 
 void List_destroy(List *list)
 {
-	LIST_FOREACH(list, first, next cur) {
+	LIST_FOREACH(list, first, next, cur) {
 		if(cur->prev){
 			free(cur->prev);
 		}
@@ -19,3 +19,100 @@ void List_destroy(List *list)
 	free(list);
 }
 
+void List_clear(List *list)
+{
+	LIST_FOREACH(list, first, next, cur){
+	free( cur->value);
+	}
+}
+
+void List_clear_destroy(List *list)
+{
+	List_clear(list);
+	List_destroy(list);
+}
+
+void List_push(List *list, void *value)
+{
+	ListNode node = calloc(1, sizeof(ListNode));
+	check_mem(node);
+
+	node->value = value;
+
+	if(list->last == NULL){
+		list->first = node;
+		list->last = node;
+	}else{
+		list->last->next = node;
+		node->prev = list->last;
+		list->last = node;
+	}
+
+	list->count++;
+error:
+	return;
+}
+
+void List_pop(List *list)
+{
+	ListNode *node = list->last;
+	return node != NULL ? List_remove(list, node) : NULL;
+}
+
+void List_unshift(List *list, void *value)
+{
+	ListNode *node = calloc(1, sizeof(ListNode));
+	check_mem(node);
+
+	node->value = value;
+
+	if(list->first == NULL){
+		list->first = node;
+		list->last = node;
+	}else{
+		node->next = list->first;
+		list->first->prev = node;
+		list->first = node;
+	}
+	
+	list->count++;
+error:
+	return;
+}
+
+void List_shift(List *list)
+{
+	ListNode *node = list->first;
+	return node != NULL ? List_remove(list, node) : NULL;
+
+void List_remove(List *list, ListNode *node)
+{
+	void *result = NULL;
+
+	check(list-first && list->last, "List is empty");
+	check(node, "Node can't ne NULL");
+
+	if(node == list->first && node == list->last){
+		list->first = NULL;
+		list->last = NULL;
+	}else if(node == list->first){
+		list->first = node->next;
+		check(list->first != NULL, "Invalid list, somehow got a first that is NULL");
+		list->first->prev = NULL;
+	}else if(node = list->last){
+		list->last = node->prev;
+		check(list->last != NULL, "Invalid list, somehow got a next that is NULL");
+		list->last->next = NULL;
+	}else{
+		ListNode *before = node->prev;
+		ListNode *after = node->after;
+		after->prev = before;
+		before->next = after;
+	}
+	
+	list->count--;
+	result = node->value;
+	free(node);
+error:
+	return result;
+}
