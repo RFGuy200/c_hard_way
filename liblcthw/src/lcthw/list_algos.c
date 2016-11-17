933d0eff5f8785154571ed684d0943a1e06c77ea
alex <akornishev@gmail.com>
2016-11-17 08:56:49 -0500

    have to use push in merge

--- liblcthw/src/lcthw/list_algos.c
+++ liblcthw/src/lcthw/list_algos.c
@@ -43,11 +43,8 @@ void merge_sort(List *list)
 	List *right = NULL;
 	int i = 0;
 	int left_count = list->count/2;
-	printf("count left = %d\n", left_count);
-	printf("count right = %d\n", list->count - left_count);
 
-
-	if (left_count > 1 || left_count == 1){
+	if (left_count > 0){
 		left = List_create();
 		right = List_create();
 		left->first = list->first;
@@ -61,13 +58,58 @@ void merge_sort(List *list)
 				left->last = cur;
 			}
 		}
+		printf("count left = %d\n", left->count);
+		printf("count right = %d\n", right->count);
 		merge_sort(left);
 		merge_sort(right);
-
-
 	}
 }
 		
 	
 			
-		
+List *merge_list(List *left, List *right)
+{
+	List *sorted = List_create();
+	ListNode *cur_left = left->first;
+	ListNode *cur_right = right->first;
+	sorted->first = malloc(sizeof(ListNode));
+	ListNode *cur_sorted = sorted->first;
+	Listnode *next = NULL;
+
+	do{
+		if(cur_left->value < cur_right->value){
+			cur_sorted->value = cur_left->value;
+			cur_left = cur_left->next;
+		}else{
+			cur_sorted->value = cur_right->value;
+			cur_right = cur_right->next;
+		}
+		next = malloc(sizeof(ListNode));
+		cur_sorted->next = next;
+	}while(cur_left != left->last && cur_right != right->last);
+
+	if(cur_left == left->last){
+		do{
+			cur_sorted->value = cur_right->value;
+			cur_right = cur_right->next;	
+			next = malloc(sizeof(ListNode));
+			cur_sorted->next = next;
+		}while(cur_right != right->last);
+	}
+
+	if(cur_right == right->last){
+		do{
+			cur_sorted->value = cur_left->value;
+			cur_left = cur_left->next;	
+			next = malloc(sizeof(ListNode));
+			cur_sorted->next = next;
+		}while(cur_left != left->last);
+	}
+	return sorted;
+}
+
+
+
+			
+			
+			
