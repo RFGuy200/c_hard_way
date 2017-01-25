411776330f93ba112c7347985a6ee0c59c8f1550
alex <akornishev@gmail.com>
2017-01-25 07:37:04 -0500

    finished ex26

--- ex26.c
+++ ex26.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "dbg.h"
 #include <assert.h>
+#include <string.h>
 
 int findlog(glob_t **log_list)
 {
@@ -10,7 +11,7 @@ int findlog(glob_t **log_list)
 	check_mem(*log_list);
 	
 	int rc = glob("/var/log/*.log", GLOB_PERIOD, NULL, *log_list);
-	check(rc != GLOB_NOMATCH, "No match found");
+	check(rc != GLOB_NOMATCH, "No log files found");
 	check(rc != GLOB_NOSPACE || rc != GLOB_ABORTED, "Glob function error");
 
 	return 0;
@@ -19,10 +20,79 @@ error:
 	return -1;	
 }
 
-void search_logs(int argc, char *argv[], glob_t **logfilesi, int *match_number)
+
+void search_logs_and(int argc, char *argv[], glob_t *logfiles)
 {
 	assert(argc > 1 && "Need at least one search parameter");
+
+	int i = 0;
+	int n = 0;
+	int file_counter = logfiles->gl_pathc;
+	char **file_list = logfiles->gl_pathv;
+	char line[1000];
+	int match = 0;
+	int file_match = 0;
 	
+	for( i = 0; i < file_counter; i++){
+		FILE *in = fopen(file_list[i], "r");
+		assert(in != NULL && "Failed to open the file");
+	
+			for( n = 1; n < argc; n++){
+				while(!feof(in)){
+					fgets(line, 999, in);
+					if(strstr(line, argv[n])){
+						match++;
+						rewind(in);
+						break;
+					}
+				}
+			}
+		if(match == argc - 1){
+			printf("%s\n", file_list[i]);
+			file_match++;
+		}
+		match = 0;
+		fclose(in);
+	}
+	if(file_match == 0)
+			printf("No files found with these search parameters\n");		
+
+}
+
+void search_logs_or(int argc, char *argv[], glob_t *logfiles)
+{
+	assert(argc > 1 && "Need at least one search parameter");
+
+	int i = 0;
+	int n = 0;
+	int file_counter = logfiles->gl_pathc;
+	char **file_list = logfiles->gl_pathv;
+	char line[1000];
+	int match = 0;
+	int file_match = 0;
+	
+	for( i = 0; i < file_counter; i++){
+		FILE *in = fopen(file_list[i], "r");
+		assert(in != NULL && "Failed to open the file");
+
+			for( n = 2; n < argc && match == 0; n++){
+				while(!feof(in)){
+					fgets(line, 999, in);
+					if(strstr(line, argv[n])){
+						match++;
+						break;
+					}
+				}
+			}
+		if(match > 0){
+			printf("%s\n", file_list[i]);
+			file_match++;
+		}
+		match = 0;
+		fclose(in);
+	}
+	if(file_match == 0)
+			printf("No files found with these search parameters\n");		
 
 }
 
@@ -30,20 +100,14 @@ int main (int argc, char *argv[])
 {
 
 	glob_t *logfiles = NULL;
-	int match_number = 0;
 
 	findlog(&logfiles);
-
-	search_logs(argc, argv, &logfiles, &match_number);
-
-	if(match_number == 0){
-			printf("No match found\n");
-	}else{
-		printf("Total number of matches: %d\n", match_number);	
-	}
 	
-
-
+	if(strstr(argv[1], "-o") && strlen(argv[1]) == 2){
+		search_logs_or(argc, argv, logfiles);
+	}else{
+		search_logs_and(argc, argv, logfiles);
+	}
 
 	free(logfiles);
 	
