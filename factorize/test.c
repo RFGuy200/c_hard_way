dc190f98443b5c02bbb55c2c9db6dcb4c90ecd18
RFGuy200 <akornishev@gmail.com>
2017-08-05 10:14:58 -0400

    added prime factors

---
+++ factorize/test.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(void)
+{
+	int array[10] = {0};
+	int test = 2;
+	int i = 0;
+	int index = 0;
+	int number = 13;  //number to factorize
+	int not_prime = 0;
+
+	assert(number > 0 && "Number must be greater than 1");
+	
+	while(number != 1){
+		if(number % test == 0){		
+				for(i = 2; i < test; i++){
+					if(test % i == 0){
+						not_prime = 1;
+						break;
+					}
+				}
+			if(not_prime == 0){
+				array[index] = test;
+				index++;	
+				number /=test;
+				test = 2;
+			}
+			not_prime = 0;
+		}else{
+			test++;
+		}
+	}
+	
+	i = 0;
+	printf("[ ");
+
+	while(array[i] != 0){
+		printf("%d ", array[i]);
+		i++;
+	}
+
+	printf("]\n");
+
+	return 0;
+
+}		
+
