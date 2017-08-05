70a05dd4d2003d35001e7b5c4991431c0bd53ca3
Alexander Kornishev <akornishev@gmail.com>
2017-08-05 10:42:25 -0400

    Update prime_factor.c

--- factorize/prime_factor.c
+++ factorize/prime_factor.c
@@ -1,3 +1,15 @@
+/* Factorize a positive integer number into its prime factors.
+
+For example:
+
+	 2 -> [2]
+	 3 -> [3]
+	 4 -> [2,2]
+	 6 -> [2,3]
+	 9 -> [3,3]
+	12 -> [2,2,3]
+	15 -> [3,5] */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
